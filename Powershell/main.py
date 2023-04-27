2c6c5a7fc38051f0b4c7933d33d36a212b622f69
CXL_edu <1351900371@qq.com>
2023-04-27 17:23:22 +0800

    Powershell

---
+++ Powershell/main.py
@@ -0,0 +1,121 @@
+from json import load
+
+import torch
+import nni
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+import nni.retiarii.strategy as strategy
+from nni.retiarii.evaluator import FunctionalEvaluator
+from nni.retiarii.experiment.pytorch import RetiariiExperiment, RetiariiExeConfig
+
+from model import Net
+
+class DotDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(DotDict, self).__init__(*args, **kwargs)
+        self.__dict__.update(*args, **kwargs)
+
+    def __getattr__(self, key):
+        value = self[key]
+        if isinstance(value, dict):
+            value = DotDict(value)
+        return value
+
+    def __getstate__(self):
+        return self.__dict__
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+
+    def update(self, *args, **kwargs):
+        super(DotDict, self).update(*args, **kwargs)
+
+def build_model(model_name: str, configs: dict):
+    model_dict = {
+        'test': 'from test import Net'
+    }
+    exec(model_dict[model_name])
+    exec('print(123,{})'.format(configs))
+    model = exec("Net(DotDict({})).float()".format(configs))
+    return model
+
+
+def train_epoch(model, device, train_loader, optimizer, epoch):
+    loss_fn = torch.nn.CrossEntropyLoss()
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test_epoch(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    accuracy = 100. * correct / len(test_loader.dataset)
+
+    print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
+          correct, len(test_loader.dataset), accuracy))
+
+    return accuracy
+
+
+def evaluate_model(model_cls):
+    # "model_cls" is a class, need to instantiate
+    model = model_cls()
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    transf = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    train_loader = DataLoader(MNIST('data/mnist', download=True, transform=transf), batch_size=64, shuffle=True)
+    test_loader = DataLoader(MNIST('data/mnist', download=True, train=False, transform=transf), batch_size=64)
+
+    for epoch in range(3):
+        # train the model for one epoch
+        train_epoch(model, device, train_loader, optimizer, epoch)
+        # test the model for one epoch
+        accuracy = test_epoch(model, device, test_loader)
+        # call report intermediate result. Result can be float or dict
+        nni.report_intermediate_result(accuracy)
+
+    # report final test result
+    nni.report_final_result(accuracy)
+
+
+if __name__ == '__main__':
+    b = load(open('setting.json'))
+    b.update({'k': 3, 'y': 32, 'configs':{}})
+    # moedl = build_model('test', b)
+    # model = Net(b).float()
+    model = Net()
+
+    search_strategy = strategy.Random(dedup=True)  # dedup=False if deduplication is not wanted
+    evaluator = FunctionalEvaluator(evaluate_model)
+    exp = RetiariiExperiment(model, evaluator, [], search_strategy)
+    exp_config = RetiariiExeConfig('local')
+    exp_config.experiment_name = 'mnist_search'
+    exp_config.max_trial_number = 4  # 最多运行 4 个实验
+    exp_config.trial_concurrency = 2  # 最多同时运行 2 个试验
+    exp_config.trial_gpu_number = 1
+    exp_config.training_service.use_active_gpu = True
+    exp.run(exp_config, 8081)
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # NNI-issue
-该仓库用于归纳本人使用NNI（Neural Network Intelligence）库进行NAS（Neural Architecture Search）时遇到的问题。This warehouse is used to summarize the problems I encountered when using the NNI (Neural Network Intelligence) library for NAS (Neural Architecture Search).
+
+
+该仓库用于归纳本人使用NNI（Neural Network Intelligence）库进行NAS（Neural Architecture Search）时遇到的问题。
+
+---
+
+This warehouse is used to summarize the problems I encountered when using the NNI (Neural Network Intelligence) library for NAS (Neural Architecture Search).
+
+
+
+- Powershell
+
+运行官方NAS的demo报错，报错提示如下ChildProcessError: Command failed: powershell.exe New-Item -Path “*my_path*” -ItemType "directory" -Force 'powershell.exe'
+
+- 
